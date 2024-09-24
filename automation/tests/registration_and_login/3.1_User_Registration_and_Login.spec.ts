ecd94c5b951d418a6acd6886f50c55bcd16ad391
datrip <datrip15@gmail.com>
2024-09-24 18:00:32 +0300

    Change test case -> test suite

--- automation/tests/registration_and_login/3.1_User_Registration_and_Login.spec.ts
+++ automation/tests/registration_and_login/3.1_User_Registration_and_Login.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../../pages/homepage';
 import { Login } from '../../pages/login';
-import { Cookies } from '../../pages/cookies';
+import { Inbox } from '../../pages/inbox';
 
 test.describe('User Registration and Login', () => {
   test('TC-UR-001: Successful Email Registration', async ({ page }) => {
     const homePage = new HomePage(page);
     const login = new Login(page);
-    const cookies = new Cookies(page);
+    const inbox = new Inbox(page);
 
     // Navigate to Wolt homepage
     await homePage.navigate();
 
     // Close cookies popup if visible
-    await cookies.closePopup();
+    await homePage.closePopup();
 
     // Open registration modal
     await homePage.openRegistrationModal();
@@ -24,5 +24,18 @@ test.describe('User Registration and Login', () => {
     // Proceed with email registration
     await login.submitEmail();
 
+    // Navigate to Inbox homepage
+    await inbox.navigateToInbox();
+
+    // Open email inbox
+    await inbox.loginInbox();
+
+    // Verify Registration email contents
+    await inbox.verifyRegistrationEmail();
+
+    // Verify Registration email link
+    await inbox.openRegistrationLink();
+
+    // Actual registration is skipped due to working with production environment and to not break the terms & conditions
   });
 });
